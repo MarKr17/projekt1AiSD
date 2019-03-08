b6e205f882fce4eb5b3c734d807c67ec0bbb8525
michaljjskawinski <48179194+michaljjskawinski@users.noreply.github.com>
2019-03-08 21:43:20 +0100

    Update projekt.cpp
    POPRAWIŁEM BŁĘDY, dobrze mierzy czas i dobrze usuwa tablice
    PS. nadal wywala przy qucisort2 dla 50000 elementow

--- projekt.cpp
+++ projekt.cpp
@@ -33,47 +33,36 @@ int main()
 	cout << "Podaj ile elementowe maja byc ciagi: ";
 	cin >> n;
 	int *tab1 = new int[n]; int *tab2 = new int[n]; int *tab3 = new int[n]; int *tab4 = new int[n]; int *tab5 = new int[n];
-	generator(tab1, tab2, tab3, tab4, tab5);
-	//WYWOLYWANIE FUNKCJI
-	/*np.
-	print(tab1-5);
-	insertion_sort(tab1-5);
-	print(tab1-5);
-	generator(tab1,tab2,tab3,tab4,tab5);   <-- zeby wygenerowac ciagi na nowo(nie byly posortowane)
-	print(tab1-5);
-	inny_sort(tab1-5);
-	print(tab1-5);
-		  ...
-	*/
-	//USTALENIE CZASU DZIALANIA ALGORYTMU
 
+
+
+	generator(tab1, tab2, tab3, tab4, tab5);
 	//SelectionSort
 	start = clock();
 	selectionSort(tab1, n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu SelectionSort 1: " << time << "ms" << endl;
-	delete[] tab1;
+	start = clock();
 	selectionSort(tab2, n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu SelectionSort 2: " << time << "ms" << endl;
-	delete[] tab2;
+	start = clock();
 	selectionSort(tab3, n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu SelectionSort 3: " << time << "ms" << endl;
-	delete[] tab3;
+	start = clock();
 	selectionSort(tab4, n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu SelectionSort 4: " << time << "ms" << endl;
-	delete[] tab4;
+	start = clock();
 	selectionSort(tab5, n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu SelectionSort 5: " << time << "ms" << endl;
-	delete[] tab5;
 
 
 	generator(tab1, tab2, tab3, tab4, tab5);
@@ -83,60 +72,58 @@ int main()
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu QuickSort 1: " << time << "ms" << endl;
-	delete[] tab1;
+	start = clock();
 	quickSort(tab2, 0, n - 1);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu QuickSort 2: " << time << "ms" << endl;
-	delete[] tab2;
+	start = clock();
 	quickSort(tab3, 0, n - 1);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu QuickSort 3: " << time << "ms" << endl;
-	delete[] tab3;
+	start = clock();
 	quickSort(tab4, 0, n - 1);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu QuickSort 4: " << time << "ms" << endl;
-	delete[] tab4;
+	start = clock();
 	quickSort(tab5, 0, n - 1);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu QuickSort 5: " << time << "ms" << endl;
-	delete[] tab5;
-    generator(tab1, tab2, tab3, tab4, tab5);
 
-//insertion_sort
+
+
+    generator(tab1, tab2, tab3, tab4, tab5);
+    //insertion_sort
 	start = clock();
 	insertion_sort(tab1);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu insertion_sort 1: " << time << "ms" << endl;
-	delete[] tab1;
+	start = clock();
 	insertion_sort(tab2);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu insertion_sort 2: " << time << "ms" << endl;
-	delete[] tab2;
+	start = clock();
 	insertion_sort(tab3);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu insertion_sort 3: " << time << "ms" << endl;
-	delete[] tab3;
+	start = clock();
 	insertion_sort(tab4);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu insertion_sort 4: " << time << "ms" << endl;
-	delete[] tab4;
+	start = clock();
 	insertion_sort(tab5);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu insertion_sort 5: " << time << "ms" << endl;
-	delete[] tab5;
 
 
-	
-	
 	generator(tab1, tab2, tab3, tab4, tab5);
 	//ShellSort
 	start = clock();
@@ -144,58 +131,58 @@ int main()
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu ShellSort 1: " << time << "ms" << endl;
-	delete[] tab1;
+	start = clock();
 	ShellSort(tab2,n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu ShellSort 2: " << time << "ms" << endl;
-	delete[] tab2;
+	start = clock();
 	ShellSort(tab3,n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu ShellSort 3: " << time << "ms" << endl;
-	delete[] tab3;
+	start = clock();
 	ShellSort(tab4,n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu ShellSort 4: " << time << "ms" << endl;
-	delete[] tab4;
+	start = clock();
 	ShellSort(tab5,n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu ShellSort 5: " << time << "ms" << endl;
-	delete[] tab5;
-	
+
+
 	generator(tab1, tab2, tab3, tab4, tab5);
-	//ShellSort
+	//HEAPSORT
 	start = clock();
 	HeapSort(tab1,n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu HeapSort 1: " << time << "ms" << endl;
-	delete[] tab1;
+    start = clock();
 	HeapSort(tab2,n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu HeapSort 2: " << time << "ms" << endl;
-	delete[] tab2;
+    start = clock();
 	HeapSort(tab3,n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu HeapSort 3: " << time << "ms" << endl;
-	delete[] tab3;
+	start = clock();
 	HeapSort(tab4,n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu HeapSort 4: " << time << "ms" << endl;
-	delete[] tab4;
+	start = clock();
 	HeapSort(tab5,n);
 	stop = clock();
 	time = (double)(stop - start);
 	cout << endl << "Czas wykonywania algorytmu HeapSort 5: " << time << "ms" << endl;
-	delete[] tab5;
 
 
+    delete[] tab1; delete[] tab2; delete[] tab3; delete[] tab4; delete[] tab5;
 }
 
 
@@ -363,61 +350,60 @@ void asc_desc(int tab[])
 }
 void ShellSort(int tab[],int n)
 {
-     for (int gap = n/2; gap > 0; gap /= 2) 
-    { 
-        
-        for (int i = gap; i < n; i += 1) 
-        { 
-           
-            int temp = tab[i]; 
-  
-            
-            int j;             
-            for (j = i; j >= gap && tab[j - gap] > temp; j -= gap) 
-                tab[j] = tab[j - gap]; 
-              
-             
-            tab[j] = temp; 
-        } 
-    } 
+     for (int gap = n/2; gap > 0; gap /= 2)
+    {
+
+        for (int i = gap; i < n; i += 1)
+        {
+
+            int temp = tab[i];
+
+
+            int j;
+            for (j = i; j >= gap && tab[j - gap] > temp; j -= gap)
+                tab[j] = tab[j - gap];
+
+
+            tab[j] = temp;
+        }
+    }
 }
 void heapify(int tab[],int n, int i)
 {
-    int largest = i; // Initialize largest as root 
-    int l = 2*i + 1; // left = 2*i + 1 
-    int r = 2*i + 2; // right = 2*i + 2 
-  
-    // If left child is larger than root 
-    if (l < n && tab[l] > tab[largest]) 
-        largest = l; 
-  
-    // If right child is larger than largest so far 
-    if (r < n && tab[r] > tab[largest]) 
-        largest = r; 
-  
-    // If largest is not root 
-    if (largest != i) 
-    { 
-        swap(tab[i], tab[largest]); 
-  
-        // Recursively heapify the affected sub-tree 
-        heapify(tab, n, largest); 
-    } 
+    int largest = i; // Initialize largest as root
+    int l = 2*i + 1; // left = 2*i + 1
+    int r = 2*i + 2; // right = 2*i + 2
+
+    // If left child is larger than root
+    if (l < n && tab[l] > tab[largest])
+        largest = l;
+
+    // If right child is larger than largest so far
+    if (r < n && tab[r] > tab[largest])
+        largest = r;
+
+    // If largest is not root
+    if (largest != i)
+    {
+        swap(tab[i], tab[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(tab, n, largest);
+    }
 }
 void HeapSort(int tab[],int n)
 {
-    // Build heap (rearrange array) 
-    for (int i = n / 2 - 1; i >= 0; i--) 
-        heapify(tab, n, i); 
-  
-    // One by one extract an element from heap 
-    for (int i=n-1; i>=0; i--) 
-    { 
-        // Move current root to end 
-        swap(tab[0], tab[i]); 
-  
-        // call max heapify on the reduced heap 
-        heapify(tab, i, 0); 
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(tab, n, i);
+
+    // One by one extract an element from heap
+    for (int i=n-1; i>=0; i--)
+    {
+        // Move current root to end
+        swap(tab[0], tab[i]);
+
+        // call max heapify on the reduced heap
+        heapify(tab, i, 0);
     }
 }
-
